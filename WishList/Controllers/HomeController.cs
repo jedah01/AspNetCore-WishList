15dcdc3559afd50dc3beee452b6f709d8392ab03
jedah01 <ongsiyi@gmail.com>
2018-07-09 00:36:46 +0930

    homecontroller direct to view page

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc; // need to use namespace
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller //nned to inherit the controller class
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
