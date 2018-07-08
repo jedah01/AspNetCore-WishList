43824bb1ca809c95682c3ab1add1a77e2ff5817b
jedah01 <ongsiyi@gmail.com>
2018-07-09 03:08:35 +0930

    create DbContext

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; //need to add this to use DbContext
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext //Dbcontext must use Microsoft.EntityFrameworkCore
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) //Add a Constructor that accepts a parameter of type DbContextOptions 
+                                                                              //named options and Invokes the base constructor as well.
+        {
+
+        }
+    }
+}
