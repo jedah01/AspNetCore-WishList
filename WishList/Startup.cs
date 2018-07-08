03610745f08bc5bd33a8219ecf9cafb4edefadd1
jedah01 <ongsiyi@gmail.com>
2018-07-09 03:56:40 +0930

    point entityframework to DbContext

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using WishList.Data;
 
 namespace WishList
 {
@@ -17,6 +18,10 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(); //add the support for MVC function 
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
+            //In the ConfigureServices method, call AddDbContext<ApplicationDbContext> on services with the 
+            //argument options => options.UseInMemoryDatabase("WishList") to point EntityFramework to the application's DbContext. 
+            //(Note: You will need to add a using statement for WishList.Data)
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
